ae86419c7aa9e6093e7ea75bfa6bbfcf580d7a0d
helenemartin <helenemartin13@hotmail.com>
2021-04-12 15:07:00 +0100

    flicker component set up

--- src/components/flicker/index.js
+++ src/components/flicker/index.js
@@ -4,7 +4,7 @@ import { Container, Item, Inner } from "./styles/flicker";
 export default function Flicker({ children, ...restProps }) {
   return (
     <Item {...restProps}>
-      <Inner direction={direction}>{children}</Inner>
+      <Inner>{children}</Inner>
     </Item>
   );
 }
