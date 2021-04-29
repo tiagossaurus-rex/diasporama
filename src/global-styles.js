c21952ac39915a82d8a5c5292f29d058c82392c9
helenemartin <helenemartin13@hotmail.com>
2021-04-29 12:05:28 +0100

    add font face to GlobalStyles file

--- src/global-styles.js
+++ src/global-styles.js
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from "styled-components";
+import YoungSerif from "../../public/fonts/YoungSerif-Regular.otf";
 
 export const GlobalStyles = createGlobalStyle`
+
+@font-face {
+    font-family: "YoungSerif";
+    src: url("./public/fonts/YoungSerif-Regular.otf") format("opentype");
+  }
     html, body {
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
