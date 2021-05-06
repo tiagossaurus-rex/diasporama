b2961b280c4451872154d0e2fe83186081bb193e
helenemartin <helenemartin13@hotmail.com>
2021-05-06 20:08:13 +0100

    Updates

--- src/helpers/postitNote.js
+++
@@ -1,4 +0,0 @@
-// @font-face {
-//     font-family: "YoungSerif";
-//     src: font-url("YoungSerif-Regular.otf") format("opentype");
-// }
--- src/pages/home.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { FlickerContainer } from "../containers/flicker";
-
-export default function Home() {
-  return <FlickerContainer />;
-}
