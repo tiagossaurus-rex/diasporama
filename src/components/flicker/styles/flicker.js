c8be12c66a02dd5f1a664ea76c9be3491835bf05
helenemartin <helenemartin13@hotmail.com>
2021-05-06 19:21:29 +0100

    add fontface

--- src/components/flicker/styles/flicker.js
+++ src/components/flicker/styles/flicker.js
@@ -10,6 +10,7 @@ export const Item = styled.div``;
 export const Container = styled.div``;
 
 export const Title = styled.h1`
+  font-family: "YoungSerif", Helvetica, Arial, sans-serif;
   font-size: 50px;
   line-height: 1.1;
   margin-bottom: 8px;
--- src/global-styles.js
+++ src/global-styles.js
@@ -1,12 +1,15 @@
 import { createGlobalStyle } from "styled-components";
-import YoungSerif from "../../public/fonts/YoungSerif-Regular.otf";
 
 export const GlobalStyles = createGlobalStyle`
 
-@font-face {
-    font-family: "YoungSerif";
-    src: url("./public/fonts/YoungSerif-Regular.otf") format("opentype");
-  }
+@font-face {font-family: "YoungSerif"; 
+src: url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.eot"); 
+src: url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.eot?#iefix") 
+format("embedded-opentype"), 
+url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.woff2") format("woff2"), 
+url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.woff") format("woff"),
+ url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.ttf") format("truetype"), 
+ url("//db.onlinewebfonts.com/t/d18acb2db67fb3aae1ccd198d37a10e9.svg#YoungSerif") format("svg"); }
     html, body {
         font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
