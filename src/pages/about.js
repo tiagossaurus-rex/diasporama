4d8012de4c8ded555a8d94ce311d6cd29f8303df
helenemartin <helenemartin13@hotmail.com>
2021-08-09 18:49:17 +0100

    clean up unsused code

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import Cheerio from "cheerio";
 
 export function About() {
   const [reviews, setReviews] = useState([]);
